68e6dbfe1b7c51c41748655efd778097c1633c56
Arnaud Patard <arnaud.patard@collabora.com>
2025-02-07 18:10:16 +0100

    arch/arm/mach-rockchip/rk3399/rk3399.c: Add gadget device to boot devices
    Define the device node used when booting over usb, allowing to
    use 'same-as-spl' boot entry.
    
    Signed-off-by: Arnaud Patard <arnaud.patard@collabora.com>

--- arch/arm/mach-rockchip/rk3399/rk3399.c
+++ arch/arm/mach-rockchip/rk3399/rk3399.c
@@ -27,6 +27,7 @@ const char * const boot_devices[BROM_LAST_BOOTSOURCE + 1] = {
 	[BROM_BOOTSOURCE_EMMC] = "/mmc@fe330000",
 	[BROM_BOOTSOURCE_SPINOR] = "/spi@ff1d0000/flash@0",
 	[BROM_BOOTSOURCE_SD] = "/mmc@fe320000",
+	[BROM_BOOTSOURCE_USB] = "/usb@fe800000/usb@fe800000",
 };
 
 static struct mm_region rk3399_mem_map[] = {
